cb0d53b617645afa948e20fabd1013252a4a1ce8
Malcolm Jeremiah <malcolmjeremiahrajeli@gmail.com>
2020-11-07 08:36:18 +0800

    Setup rspec

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,9 @@ module BlogMaster
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework  :rspec, fixtures: false
+    end
   end
 end
